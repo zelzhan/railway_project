494f28efd92b5ddf463928e62d1835bf9a989417
Abylay <44272451+abylay2018@users.noreply.github.com>
2019-11-01 03:12:51 +0600

    Update script.js

--- web/js/script.js
+++ web/js/script.js
@@ -1,14 +1,32 @@
+let routeDate;
+
 function updateRoute(items) {
-	let str = "<table class=\"table table-bordered\"><thead class=\'thead-dark\'><tr><th scope=\"col\">#</th>"
-	str += "<th scope=\"col\">Departure</th><th scope=\"col\">Destination</th><th scope=\"col\">Time</th></tr></thead>"
-	str += "<tbody>";
-	for (let i=0; i<items.length; i++) {
-		str +="<tr><th scope=\"row\">"+items[i].train_id+"</th><td>"+ items[i].dep +"</td><td>"+items[i].des + "</td><td>" + items[i].date + "</td>" +
-            "<td><button type=\"submit\" onclick='showMap(items[i])' class=\"btn btn-primary\">Show Map</button></td></tr>";
-	}
-	str += "</tbody></table>";
-	$("#table-table").html("");
-	$("#table-table").append(str);
+    let str = "<table class=\"table table-bordered\"><thead class=\'thead-dark\'><tr><th scope=\"col\">#</th>"
+    str += "<th scope=\"col\">Departure</th><th scope=\"col\">Destination</th><th scope=\"col\">Time</th><th scope=\"col\">-----</th></tr></thead>"
+    str += "<tbody>";
+    routeDate = items;
+    for (let i=0; i<items.length; i++) {
+        console.log(items[i]);
+        str +="<tr><th scope=\"row\">"+items[i].train_id+"</th><td>"+ items[i].dep +"</td><td>"+items[i].des + "</td><td>" + items[i].dateh + "</td>" +
+            "<td><button type=\"submit\" onclick ='showMap(" + i +");' class=\"btn btn-primary\">Show Map</button></td></tr>";
+    }
+    str += "</tbody></table>";
+    $("#table-table").html("");
+    $("#table-table").append(str);
+}
+
+function showMap(index) {
+    let items = routeDate[index];
+    let url = "/railway_station_service_war_exploded/services/items/" + items['dep']
+        + "/" + items['des'] + "/" + items['datey'] + "/"+items['dateh'] + "/" + items['route_id'];
+
+    $.ajax({
+        type: "GET",
+        url: url,
+        success: function (data) {
+            window.location.replace("C:\\Users\\abyl\\Desktop\\Fall 2019\\rails\\web\\map.html");
+        },
+    });
 }
 
 function sendFormRoute() {
